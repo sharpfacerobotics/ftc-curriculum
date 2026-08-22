3a94e1f83708c9f89b897f71905274839a6cb1db
srivibhavpadakandla <srivibhavpadakandla@users.noreply.github.com>
2026-08-22 21:23:08 +0000

    deploy: 7674a4d571b7a71984917ae7df8380fb80fe8acf

--- assets/js/4d41bedd.631d05a1.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunkcake_recipes=globalThis.webpackChunkcake_recipes||[]).push([[321],{30647(e){e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"mechanical"}')}}]);
---
+++ assets/js/4d41bedd.f2241485.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkcake_recipes=globalThis.webpackChunkcake_recipes||[]).push([[321],{647(e){e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"mechanical"}')}}]);
